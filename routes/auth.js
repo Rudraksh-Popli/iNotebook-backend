e4041d508f15141c4b1e946ab9e920b70ccbc06f
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-22 20:22:43 +0530

    Created Thunder Client Collections to Manage Requests

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const User = require('../models/User')
+const User = require('../models/User');
 const router = express.Router();
 
-router.post('/', [
+// Create a User using: POST "/api/auth/createuser". No Login Required
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
+    body('email', 'Enter a valid e-mail').isEmail(),
     body('password', 'Password must be atleast 8 characters long').isLength({ min: 8 })
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors, return Bad Request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user)).catch(err => {
-        console.log(err)
-        res.json({ error: 'This E-mail has already been used', message: err.message })
-    });
+    // Check whether a user with this email exists already
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "A user with this e-mail already exists!" })
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error Occurred");
+    }
 });
 
 module.exports = router;
