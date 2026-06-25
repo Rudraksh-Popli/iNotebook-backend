947c3ce9d6a82b102dc7a07b11af0f51fec4855b
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-06-25 10:16:07 +0530

    Fixed Some Issues With CORS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const connectToMongo = require('./db');
 const express = require('express');
+const cors = require('cors');
 
 connectToMongo();
+
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // Available Routes
@@ -16,5 +19,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`iNotebook-backend listening on port http://localhost:${port}`);
 });
